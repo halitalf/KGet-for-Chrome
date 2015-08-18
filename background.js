c9fdf7f3a7fe5369e5186efe9bafbace0db8d6d8
Patrick Thomas <admin@twprogrammers.com>
2015-08-18 12:09:53 -0500

    Added first time setup
    Added post-install options popup

--- background.js
+++ background.js
@@ -973,6 +973,15 @@ var download_mimetypes = [
 
 var download_extentions = [];
 
+function install_notice() {
+	if (localStorage.getItem('install_time')) return;
+
+	var now = new Date().getTime();
+	localStorage.setItem('install_time', now);
+	chrome.tabs.create({url: "options.html"});
+}
+install_notice();
+
 var user = "admin";
 var pass = "";
 var userStr = "";
